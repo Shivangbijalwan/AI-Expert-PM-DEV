718661d073e67fdf4a9318a4575477649d86ca2b
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-08-21 06:12:37 +0530

    updated functions in js

---
+++ JavaScript (Web projects)/02_function.js
@@ -0,0 +1,14 @@
+// functions 
+
+function nameofFunction(){
+    console.log("H");
+    console.log("I");
+}
+nameofFunction()
+
+
+function name(number1 , number2){
+    console.log(number1 + number2)
+
+}
+name(10,5)
