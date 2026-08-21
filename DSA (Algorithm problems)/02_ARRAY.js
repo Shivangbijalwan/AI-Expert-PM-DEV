7edd11ed2a30c1525664f59ccc664bafdc1c7b3d
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-08-22 05:11:49 +0530

    basic array and main operations

---
+++ DSA (Algorithm problems)/02_ARRAY.js
@@ -0,0 +1,19 @@
+// array is a collection of different datatypes and elements It should be inside [] square brackets.
+let arr=[];
+arr.push(100);
+arr.push(200);
+arr.push(300);
+arr.push(400);
+arr.push("shivang");
+arr.push(false);
+
+console.log(arr);
+console.log(arr[1]);
+arr.pop();
+console.log(arr);  
+
+arr[6] = 900;
+console.log(arr);
+
+
+
