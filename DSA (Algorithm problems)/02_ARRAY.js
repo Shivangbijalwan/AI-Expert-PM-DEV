87a364d6e2b76189b33d1e85a4dc3a3dbdd099ab
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-08-23 09:14:42 +0530

    // array element taken with different prompt from the user

--- DSA (Algorithm problems)/02_ARRAY.js
+++ DSA (Algorithm problems)/02_ARRAY.js
@@ -15,5 +15,11 @@ console.log(arr);
 arr[6] = 900;
 console.log(arr);
 
+// array element taken with different prompt from the user
 
+let arr = new Array();
+for (let i=0;i<arr.length;i++){
+arr[i] = Number(prompt("Enter a number for a array: "))
+};
+console.log(arr);
 
