9fe7ffe60b01e02247dba9bca8b352c021b5f106
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-08-24 04:39:40 +0530

    Questions of ARRAY in Js

--- DSA (Algorithm problems)/02_ARRAY.js
+++ DSA (Algorithm problems)/02_ARRAY.js
@@ -1,3 +1,5 @@
+
+/*
 // array is a collection of different datatypes and elements It should be inside [] square brackets.
 let arr=[];
 arr.push(100);
@@ -23,3 +25,46 @@ arr[i] = Number(prompt("Enter a number for a array: "))
 };
 console.log(arr);
 
+
+//mastering questions of array
+// move all zero to left side and one to right
+let arr =[1,1,0,1,0,1,1,0,0];
+let i=0 , j=0;
+ while(i<arr.length){
+   if(arr[i]==0){
+    let temp=arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    j++
+   }
+   i++
+ }
+ console.log(arr);
+
+
+ //ADVANCE ARRAY QUESTIONS
+
+   //left rotation by 1 element
+   
+arr=[1,2,3,4,5]
+for (let i=0; i<arr.length-1 ;i++){
+let temp = arr[i];
+arr[i] = arr[i+1];
+arr[i+1] = temp;
+};
+ 
+console.log(arr)
+*/
+
+
+//right rotation by 1 element
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = arr.length - 1; i > 0; i--) {
+    let temp = arr[i];
+    arr[i] = arr[i - 1];
+    arr[i - 1] = temp;
+}
+
+console.log(arr);
