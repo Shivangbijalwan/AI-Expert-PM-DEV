0b15fe5ca2793a4006beccd0340fd55134627068
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-08-26 15:08:43 +0530

    what is string how it looks like

---
+++ DSA (Algorithm problems)/03_strings.js
@@ -0,0 +1,4 @@
+let s = "shivang";
+console.log(s[3]);
+
+// string is a group of characters , sequence of charactor, but its not a array of a chractor , its behave like array because work with indexing.. 
