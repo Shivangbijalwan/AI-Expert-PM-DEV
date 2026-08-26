6d2cfd0f92074bac8452b7b2cbb2ce7d48b03608
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-08-26 15:09:45 +0530

    methods of string

--- DSA (Algorithm problems)/03_strings.js
+++ DSA (Algorithm problems)/03_strings.js
@@ -2,3 +2,20 @@ let s = "shivang";
 console.log(s[3]);
 
 // string is a group of characters , sequence of charactor, but its not a array of a chractor , its behave like array because work with indexing.. 
+
+
+//methods of string 
+
+console.log(s.length);
+
+console.log(s.slice(0 , 5));
+
+console.log(s.concat(" ","world"));
+
+console.log(s.trim("     erererercc           "))
+console.log(s.concat("hello"))
+
+console.log(s.charAt(2));
+console.log(s.charCodeAt(3));
+//asciicode
+
